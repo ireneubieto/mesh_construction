e08e64344c3ec5c8c0d0246b8f64f2bdd4c43a71
Irene Ubieto Alamillo <61739461+ireneubieto@users.noreply.github.com>
2021-07-01 22:16:02 +0200

    Update adjust.js

--- adjust.js
+++ adjust.js
@@ -13,7 +13,7 @@ import {
     SkinnedMesh,
     Skeleton,
     Mesh
-} from 'three.js/build/three.module.js';
+} from './three.js/build/three.module.js';
 import { OrbitControls } from 'three.js/examples/jsm/controls/OrbitControls.js';
 import { GUI } from 'three.js/examples/jsm/libs/dat.gui.module.js';
 import { GLTFLoader } from 'three.js/examples/jsm/loaders/GLTFLoader.js';
