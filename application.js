4b3c40da0f70cbc92654bad1ab73a0c8dd74418b
Irene Ubieto Alamillo <61739461+ireneubieto@users.noreply.github.com>
2021-07-02 00:16:57 +0200

    Update application.js

--- application.js
+++ application.js
@@ -14,11 +14,11 @@ import {
     BufferAttribute,
     Mesh,
     SphereGeometry
-} from './three.js/build/three.module.js';
-import { GUI } from ':/three.js/examples/jsm/libs/dat.gui.module.js';
-import { OrbitControls } from './three.js/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from './three.js/examples/jsm/loaders/GLTFLoader.js';
-import { GLTFExporter } from './three.js/examples/jsm/exporters/GLTFExporter.js';
+} from '/mesh_construction/three.js/build/three.module.js';
+import { GUI } from '/mesh_construction/three.js/examples/jsm/libs/dat.gui.module.js';
+import { OrbitControls } from '/mesh_construction/three.js/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '/mesh_construction/three.js/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFExporter } from '/mesh_construction/three.js/examples/jsm/exporters/GLTFExporter.js';
 
 var gui, scene, camera, renderer, orbit, lights;
 var face, bones, skeleton;
