67cd069941e6e590c2bc916af1e992d4eb522213
Irene Ubieto Alamillo <61739461+ireneubieto@users.noreply.github.com>
2021-07-01 22:16:30 +0200

    Update adjust.js

--- adjust.js
+++ adjust.js
@@ -14,9 +14,9 @@ import {
     Skeleton,
     Mesh
 } from './three.js/build/three.module.js';
-import { OrbitControls } from 'three.js/examples/jsm/controls/OrbitControls.js';
-import { GUI } from 'three.js/examples/jsm/libs/dat.gui.module.js';
-import { GLTFLoader } from 'three.js/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from './three.js/examples/jsm/controls/OrbitControls.js';
+import { GUI } from './three.js/examples/jsm/libs/dat.gui.module.js';
+import { GLTFLoader } from ':/three.js/examples/jsm/loaders/GLTFLoader.js';
 
 let renderer, scene, camera, gui, folder, orbit;
 var trackedMesh;
