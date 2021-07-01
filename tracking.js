55282f51f605e7a51cdd2de3e4279d8f1b8ca08c
Irene Ubieto Alamillo <61739461+ireneubieto@users.noreply.github.com>
2021-07-02 00:28:41 +0200

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,6 +1,6 @@
 import {
     Vector3
-} from './three.js/build/three.module.js';
+} from '/meshconstruction/three.js/build/three.module.js';
 
 let face_model;
 let webcam
@@ -14,7 +14,7 @@ import {
     setTargets,
     edit,
     downloadModel
-} from './application.js';
+} from '/meshconstruction/application.js';
 
 import {
     adjust,
@@ -22,7 +22,7 @@ import {
     makeAdjustMesh,
     getFinalMesh,
     stopRendering
-} from './adjust.js';
+} from '/meshconstruction/adjust.js';
 
 function sendVertex(){
     var trackedVertex = [];
